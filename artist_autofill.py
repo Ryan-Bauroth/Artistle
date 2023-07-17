4840559d23eca6c3ac23819c5322dea9237a5f48
Ryfi <65467501+Ryan-Bauroth@users.noreply.github.com>
2023-07-17 13:17:21 -0400

    Update artist_autofill.py

--- artist_autofill.py
+++ artist_autofill.py
@@ -2,8 +2,8 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 def search_artists(query):
-    client_credentials_manager = SpotifyClientCredentials(client_id='aa6fce7db87347c5b5d0690f32ad1dd1',
-                                                          client_secret='dbe95efa14544260bf8e2e1e61a29345')
+    client_credentials_manager = SpotifyClientCredentials(client_id='',
+                                                          client_secret='')
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     artist_names = []
 
