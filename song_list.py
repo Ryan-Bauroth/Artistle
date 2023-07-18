f1b3ca4f3191157c607eefaf0c0cedc9de7962b4
CemAkdurak <139142542+CemAkdurak@users.noreply.github.com>
2023-07-18 14:48:09 -0400

    fixed a problem

--- song_list.py
+++ song_list.py
@@ -10,13 +10,13 @@ def get_artist_songs(artist_name):
     artist_list = []    #creates list for songs of the chosen artist
 
 
-    results = spot.search(a=artist_name, type='artist')     #gets the artists from spotify
+    results = spot.search(q=artist_name, type='artist')     #gets the artists from spotify
 
     for item in results['artists']['items']:                #Adds the artist names to the list
         artist_list.append(item['name'])                
 
     
-    results = spot.search(a="artist:"+artist_list[0], type='track', limit=50)   #gets 50 songs from artists that include 'given artist' in their name
+    results = spot.search(q="artist:"+artist_list[0], type='track', limit=50)   #gets 50 songs from artists that include 'given artist' in their name
 
 
     for track in results['tracks']['items']:
